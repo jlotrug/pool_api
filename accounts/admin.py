5580da5dc2f66f19d7f4263ba04a36d730a886f7
Jim <jameslotruglio@gmail.com>
2023-05-17 19:12:30 -0400

    Created CustomUser forms and admin config. Created pools app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "wins",
+        "is_staff",
+    ]
+
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("wins",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None,{"fields": ("wins",)}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     first_name = models.CharField(null=True, blank=True, max_length=100)
     last_name = models.CharField(null=True, blank=True, max_length=100)
+    wins = models.IntegerField(default=0)
