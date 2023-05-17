69f5064184e76520bfc69150d35215fa24239144
Jim <jameslotruglio@gmail.com>
2023-05-17 18:24:28 -0400

    Created CustomUser model

---
+++ accounts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(null=True, blank=True, max_length=100)
+    last_name = models.CharField(null=True, blank=True, max_length=100)
