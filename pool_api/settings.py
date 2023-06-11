792d942febf252f2bb6f2392d2bf0a5333376f04
Jim <jameslotruglio@gmail.com>
2023-06-10 21:17:49 -0400

    reapplied original db config

--- pool_api/settings.py
+++ pool_api/settings.py
@@ -202,16 +202,16 @@ WSGI_APPLICATION = 'pool_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': env.dj_db_url("DATABASE_URL")
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.dj_db_url("DATABASE_URL")
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
