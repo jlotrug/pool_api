0243f2526df6ece54b5624243172ee9e55e9214e
Jim <jameslotruglio@gmail.com>
2023-06-10 21:53:46 -0400

    updated csrf token origins

--- pool_api/settings.py
+++ pool_api/settings.py
@@ -169,10 +169,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "https://football-pool.herokuapp.com",
     "http://football-pool.herokuapp.com",
-    ".herokuapp.com",
+
 ]
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "https://football-pool.herokuapp.com", ".herokuapp.com", "http://football-pool.herokuapp.com"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "https://football-pool.herokuapp.com", "http://football-pool.herokuapp.com"]
 
 ROOT_URLCONF = 'pool_api.urls'
 
